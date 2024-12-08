45989bf1115f4f1abf375e7c2947031b403aad6f
mhamry <hamrymuhammad1977@gmail.com>
2024-12-08 08:52:47 +0700

    ubah js

--- script.js
+++ script.js
@@ -34,6 +34,10 @@ $(".search-button").on("click", function () {
                 <div class="col-6 col-md-3">
                   <img src="${m.Poster}" class="img-fluid" />
                 </div>
+
+                </div>
+
+                <div class="row">
                 <div class="col-6 col-md">
                   <ul class="list-group">
                     <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
@@ -102,7 +106,7 @@ $.ajax({
                   <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
                   <li class="list-group-item"><strong>Director :</strong>${m.Director}</li>
                   <li class="list-group-item"><strong>Actors :</strong>${m.Actors}</li>
-                  <li class="list-group-item"><strong>Writer :</strong>${m.writer}</li>
+                  <li class="list-group-item"><strong>Writer :</strong>${m.Writer}</li>
                   <li class="list-group-item"><strong>Plot :</strong><br />${m.Plot}</li>
                 </ul>
               </div>
